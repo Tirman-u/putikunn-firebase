2fab2b55932427908c3334ab59f6d50f9624650a
Oscar Tirman <oscar.tirman@gmail.com>
2026-01-14 13:03:38 +0000

    Update main page to Home

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,27 +1,27 @@
 import GameResult from './pages/GameResult';
 import GroupResult from './pages/GroupResult';
+import Home from './pages/Home';
 import ManageGames from './pages/ManageGames';
 import Profile from './pages/Profile';
 import PuttingKing from './pages/PuttingKing';
 import PuttingKingOverview from './pages/PuttingKingOverview';
 import PuttingKingScoring from './pages/PuttingKingScoring';
 import PuttingKingSetup from './pages/PuttingKingSetup';
-import Home from './pages/Home';
 
 
 export const PAGES = {
     "GameResult": GameResult,
     "GroupResult": GroupResult,
+    "Home": Home,
     "ManageGames": ManageGames,
     "Profile": Profile,
     "PuttingKing": PuttingKing,
     "PuttingKingOverview": PuttingKingOverview,
     "PuttingKingScoring": PuttingKingScoring,
     "PuttingKingSetup": PuttingKingSetup,
-    "Home": Home,
 }
 
 export const pagesConfig = {
-    mainPage: "PuttingKingSetup",
+    mainPage: "Home",
     Pages: PAGES,
 };

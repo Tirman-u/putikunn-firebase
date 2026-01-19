b3892115779c4484d04a5805b0c7efb56322e3f8
Oscar Tirman <oscar.tirman@gmail.com>
2026-01-19 08:38:52 +0000

    File changes

--- src/components/putting/HostSetup.jsx
+++ src/components/putting/HostSetup.jsx
@@ -23,11 +23,7 @@ export default function HostSetup({ onStartGame, onBack, isSolo = false }) {
   const handleStart = () => {
     if (gameType === 'around_the_world') {
       // For ATW, navigate to ATW setup instead
-      if (isSolo) {
-        navigate(createPageUrl('Home') + '?mode=atw-setup&solo=1');
-      } else {
-        navigate(createPageUrl('Home') + '?mode=atw-setup&solo=0');
-      }
+      window.location.href = createPageUrl('Home') + '?mode=atw-setup&solo=' + (isSolo ? '1' : '0');
       return;
     }
     onStartGame({
